fb5bb3aef4e45481147d05237024ef65cbf9cbb9
Gustavinho Programações UAUUUUUUUUUUU <70520011+narasai10@users.noreply.github.com>
2024-07-30 16:10:23 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
+button = document.getElementById("botao")
+hobbie = document.getElementsByClassName("hobbies")
 
+
+function changeColor (){
+   document.body.style.backgroundColor = "#ffffff"; 
+   for (let i = 0; i < hobbie.length;i++){
+    hobbie[i].style.backgroundColor = "#050505"
+   }
+    
+};
+
+button.addEventListener("click", changeColor)
